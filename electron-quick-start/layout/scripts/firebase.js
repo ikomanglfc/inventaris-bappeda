759d93b99cdfcc1306c918da1e91ffc4ff9b3602
m45adiwinata <m45adiwinata@gmail.com>
2018-12-19 04:10:13 +0800

    first commit

---
+++ electron-quick-start/layout/scripts/firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyC_MBqcHrJ954euOF7nUeeshD99VoTC-8c",
+    authDomain: "inventaris-bappeda.firebaseapp.com",
+    databaseURL: "https://inventaris-bappeda.firebaseio.com",
+    projectId: "inventaris-bappeda",
+    storageBucket: "inventaris-bappeda.appspot.com",
+    messagingSenderId: "693313149186"
+};
+firebase.initializeApp(config);
